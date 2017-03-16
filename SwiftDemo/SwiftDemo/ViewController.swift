194cc80d2311d03c03968acada1ce1b93fbcb9a0
程鹏 <chengp0819@gmail.com>
2017-03-16 11:16:05 +0800

    【Feature】添加DemoGradientLayer
    Signed-off-by: 程鹏 <chengp0819@gmail.com>
    Signed-off-by: 程鹏 <chengpeng@adnonstop.com>

--- SwiftDemo/SwiftDemo/ViewController.swift
+++ SwiftDemo/SwiftDemo/ViewController.swift
@@ -13,6 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // gradientLayer
+        let gradientLayer = DemoGradientLayer()
+        
+        let view = UIView(frame: CGRect(x: 100, y: 100, width: 200, height: 200))
+        self.view.addSubview(view)
+        
+        gradientLayer.showDemoForParentView(parentView: view)
     }
 
     override func didReceiveMemoryWarning() {
