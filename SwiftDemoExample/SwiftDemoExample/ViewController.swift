013fdbaae04214a50449bae2a5b943a38072da2c
程鹏 <chengp0819@gmail.com>
2017-03-18 10:30:33 +0800

    【Feature】添加tileLayer
    Signed-off-by: 程鹏 <chengp0819@gmail.com>

--- SwiftDemoExample/SwiftDemoExample/ViewController.swift
+++ SwiftDemoExample/SwiftDemoExample/ViewController.swift
@@ -8,18 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CALayerDelegate {
+    var scrollView = UIScrollView()
+    let scale = UIScreen.main.scale
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // scrollView
+        scrollView.frame = self.view.bounds
+        scrollView.backgroundColor = UIColor.red
+        self.view.addSubview(scrollView)
+        
+        // DemoCATiledLayer
+        let tileLayer = CATiledLayer()
+        tileLayer.frame = CGRect(x: 0, y: 0, width: 2048, height: 2048)
+        tileLayer.contentsScale = scale
+        tileLayer.delegate = self
+        
+        scrollView.layer.addSublayer(tileLayer)
+        scrollView.contentSize = tileLayer.frame.size
+        tileLayer.setNeedsDisplay()
+    }
+    
+    func draw(_ layer: CALayer, in ctx: CGContext) {
+        let tileLayer = layer as! CATiledLayer
+        let bounds = ctx.boundingBoxOfClipPath
+        
+        let x = floor(bounds.origin.x / tileLayer.tileSize.width * scale)
+        let y = floor(bounds.origin.y / tileLayer.tileSize.height * scale)
+        
+        let imageName = String(format: "demo_%02i_%02i", Int(x), Int(y))  //String(format: "demo_%02d_%02d", x, y)
+        print(imageName)
+        
+        let imagePath = Bundle.main.path(forResource: imageName, ofType: "jpg")
+        let image = UIImage.init(contentsOfFile: imagePath!)
+        
+        UIGraphicsPushContext(ctx)
+        image?.draw(in: bounds)
+        UIGraphicsPopContext()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        print("didReceiveMemoryWarning")
     }
-
-
 }
 
